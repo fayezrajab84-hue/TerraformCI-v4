55ed80171acfbadc2cd842e721c642a89820d2b6
Mohammed Rajab <fayez.rajab84@gmail.com>
2026-04-07 10:04:48 +0800

    add-vm-update-tfc-org-ws

--- main.tf
+++ main.tf
@@ -13,9 +13,9 @@ terraform {
 
   # Terraform Cloud backend — workspace settings managed in TFC UI
   cloud {
-    organization = "your-tfc-organization"
+    organization = "cybervaultschool"
     workspaces {
-      name = "your-workspace-name"
+      name = "TerraformCI-v3"
     }
   }
 }
